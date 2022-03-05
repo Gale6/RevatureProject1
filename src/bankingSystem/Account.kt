b45d217842b0bbde917b5657adb5f8fff7d2fe7c
RunxiangHuang <gale6233@gmail.com>
2022-03-04 21:06:08 -0500

    added withdraw deposit transfer receive transfer to Account Class

--- src/bankingSystem/Account.kt
+++ src/bankingSystem/Account.kt
@@ -6,20 +6,36 @@ data class Account(var userName:String, var password:String, var userType:String
         this.balance -= amount
         val recording:ArrayList<String> = arrayListOf("withdraw",amount.toString(),Time.time())
         this.History.add(recording)
+        AccessObjectFile.writeToFile(this.userName+".ser",this)
+        println("withdrew $amount to ${this.userName}")
     }
 
     fun deposit(amount:Double){
         this.balance += amount
         val recording:ArrayList<String> = arrayListOf("deposit",amount.toString(),Time.time())
         this.History.add(recording)
+        AccessObjectFile.writeToFile(this.userName+".ser",this)
+        println("deposited $amount to ${this.userName}")
     }
 
     fun transfer(amount:Double, target:String){
         this.balance -= amount
+        val recording:ArrayList<String> = arrayListOf("transfer",amount.toString(),target,Time.time())
+        this.History.add(recording)
+        AccessObjectFile.writeToFile(this.userName+".ser",this)
+        println("transferred $amount to $target")
+
+        //call target object file
+        val targetObject = AccessObjectFile.readFromFile("$target.ser")
+        targetObject.receiveTransfer(amount,this.userName)
+        AccessObjectFile.writeToFile("$target.ser",targetObject)
     }
 
     fun receiveTransfer(amount: Double,sender:String){
-        //TODO
+        this.balance += amount
+        val recording:ArrayList<String> = arrayListOf("receive",amount.toString(),sender,Time.time())
+        this.History.add(recording)
+        println("received $amount from $sender")
     }
 
 
